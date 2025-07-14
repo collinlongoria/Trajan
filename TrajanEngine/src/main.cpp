8ec71970b8f7fa6eae722dacfa5f4b5b39df422a
Collin Longoria <collin.longoria@digipen.edu>
2025-07-14 12:40:10 -0700

    0.0.1
    ## [0.0.1] - 2025-07-14
    ### Added
    - CMakeLists for compiling
    - Test main file
    - GLFW Submodule setup
    - (this is essentially a first commit - testing branches)

---
+++ TrajanEngine/src/main.cpp
@@ -0,0 +1,73 @@
+//
+// Created by colli on 7/14/2025.
+//
+#include <iostream>
+
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+int main(void) {
+    // 1. Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW.\n";
+        return -1;
+    }
+
+    // 2. Disable OpenGL context (we're using Vulkan)
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    // 3. Create window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Trajan Engine", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window.\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    // 4. Create Vulkan instance
+    VkInstance instance;
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "TrajanEngine";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "Trajan";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_3;
+
+    // Get required extensions from GLFW
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        std::cerr << "Failed to create Vulkan instance.\n";
+        return -1;
+    }
+
+    // 5. Create Vulkan surface using GLFW (cross-platform)
+    VkSurfaceKHR surface;
+    if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+        std::cerr << "Failed to create Vulkan surface.\n";
+        return -1;
+    }
+
+    std::cout << "Window and Vulkan surface created successfully!\n";
+
+    // 6. Main loop (empty)
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    // 7. Cleanup
+    vkDestroySurfaceKHR(instance, surface, nullptr);
+    vkDestroyInstance(instance, nullptr);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
